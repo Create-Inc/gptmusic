fc4d5ce24ec143b1292cfde5d04e18034beea928
Marcus Lowe <marcus@create.xyz>
2023-11-10 11:02:33 -0800

    cleanup

--- config/site.ts
+++ config/site.ts
@@ -1,18 +1,6 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js",
-  description:
-    "Beautifully designed components built with Radix UI and Tailwind CSS.",
-  mainNav: [
-    {
-      title: "Home",
-      href: "/",
-    },
-  ],
-  links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn/ui",
-    docs: "https://ui.shadcn.com",
-  },
+  name: "GPT Plays Jazz",
+  description: "GPT4 plays jazz",
 }
