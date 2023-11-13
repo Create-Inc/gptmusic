23d35f929d128e1f09ee5e1f28281e26765dbe09
Marcus Lowe <marcus@create.xyz>
2023-11-12 21:58:59 -0800

    feat: improvements

--- app/page.tsx
+++ app/page.tsx
@@ -389,26 +389,64 @@ export default function IndexPage() {
           MusicGPT
         </h1>
         <h2 className="mb-6 text-lg tracking-tighter">
-          a piano song that never ends
+          unique every time and <strong>never ends</strong>
         </h2>
         <div className="flex gap-4">
-          <Button
-            onClick={() => {
-              handlePause();
-            }}
-            disabled={!playing}
-          >
-            Pause
-          </Button>
-
-          <Button
-            onClick={() => {
-              handlePlay();
-            }}
-            disabled={playing}
-          >
-            Play
-          </Button>
+          {isLoading ? (
+            <svg
+              height="20"
+              width="20"
+              xmlns="http://www.w3.org/2000/svg"
+              className="animate-spin"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              />
+            </svg>
+          ) : playing ? (
+            <button
+              onClick={() => {
+                handlePause();
+              }}
+            >
+              <svg
+                width="15"
+                height="20"
+                viewBox="0 0 15 22"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M14.332 21V1"
+                  stroke="white"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M1 21V1"
+                  stroke="white"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </button>
+          ) : (
+            <button
+              onClick={() => {
+                handlePlay();
+              }}
+            >
+              <svg width="20" height="20" viewBox={`0 0 20 20`}>
+                <path
+                  fill="currentColor"
+                  d="M5.135 2.621a.25.25 0 0 0-.385.21v14.337a.25.25 0 0 0 .385.21l11.15-7.168a.25.25 0 0 0 0-.42L5.136 2.62zm-1.885.21c0-1.384 1.532-2.22 2.696-1.471l11.151 7.168a1.75 1.75 0 0 1 0 2.944L5.947 18.64c-1.165.75-2.697-.087-2.697-1.472V2.832z"
+                />
+              </svg>
+            </button>
+          )}
         </div>
       </section>
       <section>
